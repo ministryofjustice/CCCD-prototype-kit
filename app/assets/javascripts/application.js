494fc1124cb2175409c8cc022767425e50879ff5
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-09-08 13:59:42 +0100

    Static pages for external user forms
    - boilerplate
    - structure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
   // Show and hide toggled content
   // Where .multiple-choice uses the data-target attribute
   // to toggle hidden content
+  console.log('HELLOO');
   var showHideContent = new GOVUK.ShowHideContent()
   showHideContent.init()
 })
--- server.js
+++ server.js
@@ -113,6 +113,7 @@ app.locals.cookieText = config.cookieText
 app.locals.promoMode = promoMode
 app.locals.releaseVersion = 'v' + releaseVersion
 app.locals.serviceName = config.serviceName
+app.locals.homepage_url = config.homepageURL
 
 // Support session data
 app.use(session({

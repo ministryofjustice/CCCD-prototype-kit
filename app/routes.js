54ab1d43f63eee4dea5d1dc0d16cfab90f69d92b
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-10-06 13:55:51 +0100

    Offence details: page, macros, data, route

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var data = require('./data.json')
+var _ = require('underscore')
+
 
 // Route index page
 router.get('/', function (req, res) {
@@ -8,14 +10,28 @@ router.get('/', function (req, res) {
 })
 
 // Route agfs page
-router.get('/examples/agfs', function (req, res) {
-  res.render('examples/agfs/index', data.routes['agfs/index'])
+router.get('/examples/agfs/case-details', function (req, res) {
+  res.render('examples/agfs/case-details', data.routes['agfs/case-details'])
 })
 
 // Route agfs page
 router.get('/examples/agfs/defendants', function (req, res) {
-  console.log(req.session.data);
   res.render('examples/agfs/defendants', data.routes['agfs/defendants'])
+})
+
+
+// Route agfs page
+router.get('/examples/agfs/offence', function (req, res) {
+
+  // read a map of ids to data store names
+  var dataMap = data.routes['agfs/offence'].formData.objRefs;
+
+  // loop over the map and attach the data lists to the options key
+  _.each(dataMap, function(obj, idx, list){
+    data.routes['agfs/offence'].formData[idx].options = data.lists[list[idx]];
+  });
+
+  res.render('examples/agfs/offence', data.routes['agfs/offence'])
 })
 
 

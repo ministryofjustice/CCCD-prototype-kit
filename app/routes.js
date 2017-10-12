8fca8d100c93168a3b6d94a1b3d476e65aaccbe8
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-10-12 13:16:26 +0100

    Refactor before LGFS work

--- app/routes.js
+++ app/routes.js
@@ -1,85 +1,20 @@
 var express = require('express')
 var router = express.Router()
 var data = require('./data.json')
-var _ = require('underscore')
-
+var utils = require('./utils')
 
 // Route index page
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.render('examples/index')
 })
 
-router.get('/examples/agfs/case-details', function (req, res) {
-  res.render('examples/agfs/case-details', data.routes['agfs/case-details'])
+// AGFS router and data loading.
+router.get('/examples/agfs/*', function(req, res) {
+  var path = req.path.substring(1);
+  var lookup = path.replace('examples/', '')
+  res.render(path, utils.loadData(lookup, data))
 })
 
-router.get('/examples/agfs/defendants', function (req, res) {
-  res.render('examples/agfs/defendants', data.routes['agfs/defendants'])
-})
-
-router.get('/examples/agfs/offence', function (req, res) {
-
-  // read a map of ids to data store names
-  var dataMap = data.routes['agfs/offence'].formData.objRefs;
-
-  // loop over the map and attach the data lists to the options key
-  _.each(dataMap, function(obj, idx, list){
-    data.routes['agfs/offence'].formData[idx].options = data.lists[list[idx]];
-  });
-
-  res.render('examples/agfs/offence', data.routes['agfs/offence'])
-})
-
-router.get('/examples/agfs/fixed-fees', function (req, res) {
-
-  // read a map of ids to data store names
-  var dataMap = data.routes['agfs/fixed-fees'].formData.objRefs;
-
-  // loop over the map and attach the data lists to the options key
-  _.each(dataMap, function(obj, idx, list){
-    data.routes['agfs/fixed-fees'].formData[idx].options = data.lists[list[idx]];
-  });
-
-  res.render('examples/agfs/fixed-fees', data.routes['agfs/fixed-fees'])
-})
-
-router.get('/examples/agfs/misc-fees', function (req, res) {
-
-  // read a map of ids to data store names
-  var dataMap = data.routes['agfs/misc-fees'].formData.objRefs;
-
-  // loop over the map and attach the data lists to the options key
-  _.each(dataMap, function(obj, idx, list){
-    data.routes['agfs/misc-fees'].formData[idx].options = data.lists[list[idx]];
-  });
-
-  res.render('examples/agfs/misc-fees', data.routes['agfs/misc-fees'])
-})
-
-router.get('/examples/agfs/expenses', function (req, res) {
-
-  // read a map of ids to data store names
-  var dataMap = data.routes['agfs/expenses'].formData.objRefs;
-
-  // loop over the map and attach the data lists to the options key
-  _.each(dataMap, function(obj, idx, list){
-    data.routes['agfs/expenses'].formData[idx].options = data.lists[list[idx]];
-  });
-
-  res.render('examples/agfs/expenses', data.routes['agfs/expenses'])
-})
-
-router.get('/examples/agfs/supporting-evidence', function (req, res) {
-  // read a map of ids to data store names
-  var dataMap = data.routes['agfs/supporting-evidence'].formData.objRefs;
-
-  // loop over the map and attach the data lists to the options key
-  _.each(dataMap, function(obj, idx, list){
-    data.routes['agfs/supporting-evidence'].formData[idx].options = data.lists[list[idx]];
-  });
-
-  res.render('examples/agfs/supporting-evidence', data.routes['agfs/supporting-evidence'])
-})
 
 // add your routes here
 module.exports = router
---
+++ app/utils.js
@@ -0,0 +1,22 @@
+var _ = require('underscore')
+module.exports = {
+  loadData: function(pointer, data) {
+    // read a map of ids to data store names
+    var dataMap = data.routes[pointer];
+
+    if (dataMap) {
+
+      if (dataMap.loaded) {
+        return data.routes[pointer];
+      }
+
+      // loop over the map and attach the data lists to the options key
+      _.each(dataMap.formData.objRefs, function(obj, idx, list) {
+        data.routes[pointer].formData[idx].options = data.lists[list[idx]];
+      });
+      dataMap.loaded = true;
+      return data.routes[pointer];
+    }
+    return data.routes[pointer] || {};
+  }
+}

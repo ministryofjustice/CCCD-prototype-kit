d4197469ecd5fbf71fabab594643575200e8bb53
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-11-20 11:09:16 +0000

    Claims list + copy amends + offence ctrl

--- app/assets/javascripts/modules/moj.Modules.OffenceDetails.js
+++ app/assets/javascripts/modules/moj.Modules.OffenceDetails.js
@@ -6,7 +6,18 @@ moj.Modules.OffenceDetails = {
   bindEvents: function() {
     // Offence Category random chooser
     $.subscribe('general-select-offence-category-1', function(e, data) {
-      $("select#general-select-offence-class-1").prop('selectedIndex', _.sample([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]));
+
+      if(data.value === "Murder"){
+        $("select#general-select-offence-class-1").prop('selectedIndex', _.sample([1]));
+        return;
+      }
+
+      if(data.value === "Possession of offensive weapon"){
+        $("select#general-select-offence-class-1").prop('selectedIndex', _.sample([8]));
+        return;
+      }
+
+      $("select#general-select-offence-class-1").prop('selectedIndex', _.sample([2, 3, 4, 5, 6, 7, 9, 10, 11]));
     })
   }
 }

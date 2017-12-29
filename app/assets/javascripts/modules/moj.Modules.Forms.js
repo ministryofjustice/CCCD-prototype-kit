98c818f0c08e2b6cd6e85be2e0194e68a40e2a1d
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-12-29 13:13:26 +0000

    Tweaks

--- app/assets/javascripts/modules/moj.Modules.Forms.js
+++ app/assets/javascripts/modules/moj.Modules.Forms.js
@@ -25,26 +25,35 @@ moj.Modules.Form = {
 
       // add up all the fees
       $('.fx-calculate.fx-fees').each(function(idx, el) {
-        feesArr.push(parseFloat($(el).val() || 0));
-      });
-
-      $('.fx-calculate.fx-vat').each(function(idx, el) {
-        vatArr.push(parseFloat($(el).val() || 0));
+        var amount = parseFloat($(el).val() || 0);
+        feesArr.push(amount);
+        vatArr.push(amount*0.2);
       });
 
       $('.fx-calculate.fx-expense').each(function(idx, el) {
-        expenseArr.push(parseFloat($(el).val() || 0));
+        var amount = parseFloat($(el).val() || 0);
+        expenseArr.push(amount);
+        vatArr.push(amount*0.2);
       });
 
       $('.fx-calculate.fx-disbursement').each(function(idx, el) {
-        distbursementArr.push(parseFloat($(el).val() || 0));
+        var amount = parseFloat($(el).val() || 0);
+        distbursementArr.push(amount);
+        vatArr.push(amount*0.2);
       });
+
+      $('.fx-calculate.fx-vat').each(function(idx, el) {
+
+        console.log('skipped', parseFloat($(el).val() || 0));
+      });
+
+      console.log(vatArr.reduce(reducer));
 
       $('[name="' + context + '-fee"]').val(feesArr.reduce(reducer))
       $('[name="' + context + '-vat"]').val(vatArr.reduce(reducer))
       $('[name="' + context + '-expenses"]').val(expenseArr.reduce(reducer))
       $('[name="' + context + '-disbursements"]').val(distbursementArr.reduce(reducer))
-
+      // e.preventDefault();
     });
 
   },

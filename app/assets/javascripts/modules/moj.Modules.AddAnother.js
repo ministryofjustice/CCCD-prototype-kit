f8b56b2021688b8797f531e1a835c009e3fff85b
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-02-05 15:55:17 +0000

    Prototype: AGFS Grad fees alternatives
    PT:
    https://www.pivotaltracker.com/story/show/154925221
    
    Updated current version
    Created alt version

--- app/assets/javascripts/modules/moj.Modules.AddAnother.js
+++ app/assets/javascripts/modules/moj.Modules.AddAnother.js
@@ -3,12 +3,12 @@ moj.Modules.AddAnother = {
     $('.fx-addmore').on('click', '.button, a', function(e) {
 
       var selector = $(e.delegateTarget).data('selector');
-      console.log(selector);
+      var $parent = $(e.delegateTarget).parent();
       $($(selector).not(':visible')[0]).removeClass('hidden');
 
       if (!($(selector).not(':visible')[0])) {
-        $('.fx-addmore').remove()
-        $('.fx-duplicate').remove()
+        $parent.find('.fx-addmore').remove();
+        $parent.find('.fx-duplicate').remove();
       }
 
     });

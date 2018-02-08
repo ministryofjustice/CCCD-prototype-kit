fc911e31d110ca5e86512439bc29ba58ab4379ba
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-02-08 10:45:40 +0000

    Offence New WIP

--- app/utils.js
+++ app/utils.js
@@ -1,5 +1,28 @@
 var _ = require('underscore')
+var offences = require('./assets/javascripts/data/scheme-9-offences.json')
+
 module.exports = {
+  getOffences: function() {
+    return offences
+  },
+
+  getOffenceClass: function(class_id) {
+
+    var classObj = this.getOffences().classes.filter(function(el) {
+      return (el.id == class_id)
+    })
+
+    return classObj[0]
+  },
+
+  getOffenceCategory: function(class_id, category_id) {
+
+    var classObj = this.getOffenceClass(class_id).categories.filter(function(el) {
+      return (el.id == category_id)
+    })
+
+    return classObj[0]
+  },
   feetypeLookup: function(string, scheme) {
     var data = {
       // AGFS case types
